348004a899f8141496a95e6f21d5d3b48b433cec
ahbpo <aaro.p.salminen@gmail.com>
2026-03-15 21:28:03 +0200

    initial commit

---
+++ automount.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+device=$1
+mountpoint=$2
+is_root=0
+if [ "$(whoami)" == "root" ]; then
+	echo "root"
+	is_root=1
+fi
+
+if [ ! -b "$device" ]; then
+	echo "not a valid device file"
+	exit 1
+elif [ ! -d "$mountpoint" ]; then
+	echo "mount point is not valid"
+	exit 1
+fi
