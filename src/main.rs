422743b8bfd20eb2de8651ecc5b29a8978041fb4
ahbpo <aaro.p.salminen@gmail.com>
2026-05-02 13:08:00 +0300

    added some comments

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ use colorful::Colorful;
 fn main() {
     let mut no_whoami = false;
     let arguments: Vec<String> = args().collect();
+
+    // making sure that at least 2 arguments are supplied
+    // before setting them to variables
     if arguments.len() < 2 {
         println!("{} Please enter the device and mountpoint {}", "error:".red(), "(eg. automount /dev/sda1 /mnt/)".dark_gray());
         std::process::exit(1);
@@ -17,12 +20,14 @@ fn main() {
         .stdout(Stdio::piped())
         .output();
 
+    // everyone has whoami this is the most pointless thing ever
+    // you could've just ran with sudo when whoami fails
+    // but noo you had to implement a failsafe
     if user.is_ok() != true {
         println!("whoami is not installed, trying id -un");
         let user = Command::new("id").arg("-un").output();
         no_whoami = true;
     }
-
     if user.is_ok() != true && no_whoami {
         println!("id -un failed, running as sudo even if you are root");
     }
@@ -30,6 +35,7 @@ fn main() {
     let mut user = String::from_utf8(user.unwrap().stdout).unwrap();
     user.pop();
     dbg!("{}", user);
+    // user now contains just the current user name (without the trailing newline)
 
     dbg!("{} {} {}", &arguments, device, mountpoint);
 }
