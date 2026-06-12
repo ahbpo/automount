99ce98685319fc9b4253bb38c93023becbe68bee
ahbpo <aaro.p.salminen@gmail.com>
2026-06-12 21:33:19 +0300

    add variable run_with_sudo, grammatical "correction"

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
-use std::process::{Command, Stdio};
-use std::env::args;
 use colorful::Colorful;
+use std::env::args;
+use std::process::{Command, Stdio};
 
 fn main() {
-    let mut no_whoami = false;
+    let mut run_with_sudo: bool;
+    let mut no_whoami: bool = false;
     let arguments: Vec<String> = args().collect();
 
     // making sure that at least 2 arguments are supplied
     // before setting them to variables
     if arguments.len() < 2 {
-        println!("{} Please enter the device and mountpoint {}", "error:".red(), "(eg. automount /dev/sda1 /mnt/)".dark_gray());
+        println!(
+            "{} please enter the device and mountpoint {}",
+            "error:".red(),
+            "(eg. automount /dev/sda1 /mnt/)".dark_gray()
+        );
         std::process::exit(1);
     }
 
@@ -34,8 +39,13 @@ fn main() {
 
     let mut user = String::from_utf8(user.unwrap().stdout).unwrap();
     user.pop();
-    dbg!("{}", user);
+    dbg!("{}", &user);
     // user now contains just the current user name (without the trailing newline)
 
+    if &user != "root" {
+        run_with_sudo = true;
+    } else {
+        run_with_sudo = false;
+    }
     dbg!("{} {} {}", &arguments, device, mountpoint);
 }
